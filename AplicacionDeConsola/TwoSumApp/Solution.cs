64854ddfc182e8aadfda0e4bdddc8425b5256b75
Heyler Montoya <Hmontoya@wigilabs.com>
2025-09-26 09:11:02 -0500

    Se sube la aplicacion de consola

---
+++ AplicacionDeConsola/TwoSumApp/Solution.cs
@@ -0,0 +1,29 @@
+﻿public class Solution
+{
+    public int[] TwoSum(int[] nums, int target)
+    {
+        Dictionary<int, int> dictionary = [];
+        for (int i = 0; i < nums.Length; i++)
+        {
+            int complement = target - nums[i];
+            if (dictionary.ContainsKey(complement))
+            {
+                return [dictionary[complement], i];
+            }
+            dictionary[nums[i]] = i;
+        }
+        return [];
+    }
+
+    public static void Main()
+    {
+        var solution = new Solution();
+
+        int[] nums = { 2, 7, 11, 15 };
+        int target = 9;
+
+        int[] result = solution.TwoSum(nums, target);
+
+        Console.WriteLine($"Output: [{string.Join(",", result)}]");
+    }
+}
